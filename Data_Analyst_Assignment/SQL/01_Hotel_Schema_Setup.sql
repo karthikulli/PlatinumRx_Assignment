f9023f9f12d6a6d897ec1c198ffb90c01487c81f
karthikulli <ullikarthik6@gmail.com>
2026-04-09 10:15:27 +0530

    Create initial hotel schema with necessary tables

---
+++ Data_Analyst_Assignment/SQL/01_Hotel_Schema_Setup.sql
@@ -0,0 +1,38 @@
+-- USERS TABLE
+CREATE TABLE users (
+    user_id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(100),
+    phone_number VARCHAR(15),
+    mail_id VARCHAR(100),
+    billing_address VARCHAR(255)
+);
+
+-- BOOKINGS TABLE
+CREATE TABLE bookings (
+    booking_id VARCHAR(50) PRIMARY KEY,
+    booking_date TIMESTAMP,
+    room_no VARCHAR(50),
+    user_id VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+-- BOOKING COMMERCIALS TABLE
+CREATE TABLE booking_commercials (
+    id VARCHAR(50) PRIMARY KEY,
+    booking_id VARCHAR(50),
+    bill_id VARCHAR(50),
+    bill_date TIMESTAMP,
+    item_id VARCHAR(50),
+    item_quantity DECIMAL(10,2),
+    FOREIGN KEY (booking_id) REFERENCES bookings(booking_id),
+    FOREIGN KEY (item_id) REFERENCES items(item_id)
+);
+
+
+-- ITEMS TABLE
+CREATE TABLE items (
+    item_id VARCHAR(50) PRIMARY KEY,
+    item_name VARCHAR(100),
+    item_rate DECIMAL(10,2)
+);

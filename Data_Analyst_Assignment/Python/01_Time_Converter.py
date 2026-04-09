92d5ee0292dfa5e401a6e3636906129ab2d632c7
karthikulli <ullikarthik6@gmail.com>
2026-04-09 11:34:47 +0530

    Refactor time conversion to use input directly

---
+++ Data_Analyst_Assignment/Python/01_Time_Converter.py
@@ -0,0 +1,11 @@
+minutes=int(input())
+hours = minutes // 60
+mins = minutes % 60
+if hours > 0 and mins > 0:
+    return f"{hours} hr{'s' if hours>1 else ''} {mins} minute{'s' if mins>1 else ''}"
+elif hours > 0:
+    return f"{hours} hr{'s' if hours>1 else ''}"
+else:
+    return f"{mins} minute{'s' if mins>1 else ''}"
+
+

b9276e1c032d828f83ebdd075d69fe6c259f7795
karthikulli <ullikarthik6@gmail.com>
2026-04-09 11:39:23 +0530

    Given minutes to Time converter

--- Data_Analyst_Assignment/Python/01_Time_Converter.py
+++ Data_Analyst_Assignment/Python/01_Time_Converter.py
@@ -2,10 +2,10 @@ minutes=int(input())
 hours = minutes // 60
 mins = minutes % 60
 if hours > 0 and mins > 0:
-    return f"{hours} hr{'s' if hours>1 else ''} {mins} minute{'s' if mins>1 else ''}"
+    print(f"{hours} hr{'s' if hours>1 else ''} {mins} minute{'s' if mins>1 else ''}")
 elif hours > 0:
-    return f"{hours} hr{'s' if hours>1 else ''}"
+    print(f"{hours} hr{'s' if hours>1 else ''}")
 else:
-    return f"{mins} minute{'s' if mins>1 else ''}"
+    print(f"{mins} minute{'s' if mins>1 else ''}")
 
 
